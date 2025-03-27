d92876ba722926c09e5c6d2c7e8a735f852afd04
nivbadli1 <nivbdbd@gmail.com>
2025-03-27 20:01:44 +0200

    Update consumer_basic.py

--- consumer_basic.py
+++ consumer_basic.py
@@ -1,51 +1,44 @@
 #%%
-from confluent_kafka import Consumer, KafkaException, KafkaError
+from kafka import KafkaConsumer
+from kafka.errors import KafkaError
 
-# Create Consumer instance
-consumer_1 = Consumer({
-    'bootstrap.servers': 'localhost:29092',
-    'group.id': 'mygroup1',
-    'auto.offset.reset': 'earliest'
-})
+# Create KafkaConsumer instance
+consumer_1 = KafkaConsumer(
+    'kafka_python_test',
+    bootstrap_servers='course-kafka:9092',
+    group_id='mygroup1',
+    auto_offset_reset='earliest',
+    value_deserializer=lambda x: x.decode('utf-8')
+)
 
-
-
+# Alternative configuration example (commented out)
 """
-consumer_config = {
-    'bootstrap.servers': 'localhost:9092',
-    'group.id': 'my_consumer_group',
-    'auto.offset.reset': 'earliest',
-    'enable.auto.commit': True,
-    'auto.commit.interval.ms': 5000,
-    'session.timeout.ms': 6000,
-    'key.deserializer': StringDeserializer('utf_8'),
-    'value.deserializer': StringDeserializer('utf_8'),
-}
-
+consumer = KafkaConsumer(
+    bootstrap_servers='course-kafka:9092',
+    group_id='my_consumer_group',
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    auto_commit_interval_ms=5000,
+    session_timeout_ms=6000,
+    key_deserializer=lambda x: x.decode('utf-8') if x else None,
+    value_deserializer=lambda x: x.decode('utf-8') if x else None
+)
 """
-
-# Subscribe to topic
-consumer_1.subscribe(['kafka_python_test'])
 
 try:
-    while True:
-        print("Polling for messages from topic: kafka_python_test")
-        msg = consumer_1.poll(1.0)  # Wait for a message up to 1 second
-        if msg is None:
-            continue
-        if msg.error():
-            if msg.error().code() == KafkaError._PARTITION_EOF:
-                # End of partition event
-                print('%% %s [%d] reached end at offset %d\n' %
-                      (msg.topic(), msg.partition(), msg.offset()))
-            elif msg.error():
-                raise KafkaException(msg.error())
-        else:
-            # Proper message
-            print('Received message: {}'.format(msg.value().decode('utf-8')))
+    print("Polling for messages from topic: kafka_python_test")
+    
+    # In kafka-python, consumption is done by iterating over the consumer
+    for message in consumer_1:
+        try:
+            # Print the received message
+            print(f'Received message: {message.value}')
+            print(f'Topic: {message.topic}, Partition: {message.partition}, Offset: {message.offset}')
+        except Exception as e:
+            print(f'Error processing message: {e}')
+
 except KeyboardInterrupt:
     pass
 finally:
-    # Close down consumer to commit final offsets.
+    # Close the consumer
     consumer_1.close()
-
