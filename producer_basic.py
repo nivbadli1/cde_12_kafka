b689ec596cb708adceaa573bab78ab574904f0bb
nivbadli1 <nivbdbd@gmail.com>
2025-03-27 20:01:57 +0200

    Update producer_basic.py

--- producer_basic.py
+++ producer_basic.py
@@ -1,32 +1,43 @@
 #%%
-from confluent_kafka import Producer
+from kafka import KafkaProducer
 
-
+# Producer configuration options
 producer_config = {
-    'bootstrap.servers': 'localhost:29092',
-    'client.id': 'my-producer',
+    'bootstrap_servers': 'course-kafka:9092',
+    'client_id': 'my-producer',
     'acks': 'all',
     'retries': 5,
-    'batch.size': 16384,
-    'linger.ms': 5,
-    'compression.type': 'gzip',
-    'max.request.size': 1048576
+    'batch_size': 16384,
+    'linger_ms': 5,
+    'compression_type': 'gzip',
+    'max_request_size': 1048576
 }
-p  = Producer({'bootstrap.servers': 'localhost:29092'})
-def delivery_report(err, msg):
-    print(msg)
-    if err is not None:
-        print('Message delivery failed: {}'.format(err))
-    else:
-        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
-# Create Producer instance
-producer_1 = Producer({'bootstrap.servers': 'localhost:29092'})
+# Producer with full configuration
+producer = KafkaProducer(
+    bootstrap_servers='course-kafka:9092',
+    client_id='my-producer',
+    acks='all',
+    retries=5,
+    batch_size=16384,
+    linger_ms=5,
+    compression_type='gzip',
+    max_request_size=1048576,
+    value_serializer=lambda v: str(v).encode('utf-8')
+)
 
+# Simpler producer with basic configuration
+producer_1 = KafkaProducer(
+    bootstrap_servers='course-kafka:9092',
+    value_serializer=lambda v: str(v).encode('utf-8')
+)
+
+# Send messages
 for i in range(20):
-    producer_1.produce('kafka_python_test', f'hello, world from cde_12 msg number {i}', callback=delivery_report)
+    future = producer_1.send('kafka_python_test', f'hello, world from cde_12 msg number {i}')
+    # Get metadata for the sent message
+    record_metadata = future.get(timeout=10)
+    print(f'Message delivered to {record_metadata.topic} [{record_metadata.partition}] at offset {record_metadata.offset}')
 
-
-# Wait for any outstanding messages to be delivered and delivery report callbacks to be triggered.
+# Wait for any outstanding messages to be delivered
 producer_1.flush()
-
